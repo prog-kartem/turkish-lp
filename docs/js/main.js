63f584ab30010866a22fd9576a3ebcd2576ee85b
prog-kartem <prog.kartem@gmail.com>
2020-12-12 14:07:13 +0200

    added adaptive for utility and fix pc version

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,7 +2,8 @@ let utilitySlider = new Swiper('.utility__slider', {
   wrapperClass: 'utility__wrapper',
   slideClass: 'utility__slide',
   spaceBetween: 30,
-  slidesPerView: 3,
+  slidesPerView: 'auto',
+  loop: true,
   navigation: {
     nextEl: '.utility__btns .slider-btns__btn:last-child',
     prevEl: '.utility__btns .slider-btns__btn:first-child',
