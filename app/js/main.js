6155d9a98857781b90ce330cfcf839d18e15c27c
prog-kartem <prog.kartem@gmail.com>
2020-12-12 21:42:51 +0200

    added test modal window

--- app/js/main.js
+++ app/js/main.js
@@ -23,3 +23,33 @@ let skillsSlider = new Swiper('.skills__slider', {
     prevEl: '.skills__btns .slider-btns__btn:first-child',
   },
 });
+
+const btnsInfo = document.querySelectorAll('.btn');
+const modalOverlay = document.querySelector('.modals__overlay');
+const modals = document.querySelectorAll('.modals__modal');
+const modalClose = document.querySelector('.modals__close');
+
+btnsInfo.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    let path = e.currentTarget.getAttribute('data-path');
+
+    modals.forEach((el) => {
+      el.classList.remove('modals__modal--visible');
+    });
+
+    document.querySelector(`[data-target="${path}"]`).classList.add('modals__modal--visible');
+    document.querySelector('body').style.overflow = 'hidden';
+    modalOverlay.classList.add('modals__overlay--visible');
+  });
+});
+
+modalOverlay.addEventListener('click', (e) => {
+  console.log(e.target);
+  if (e.target == modalOverlay || e.target == modalClose) {
+    modalOverlay.classList.remove('modals__overlay--visible');
+    document.querySelector('body').style.overflow = 'visible';
+    modals.forEach((el) => {
+      el.classList.remove('modals__modal--visible');
+    });
+  }
+});
